754730ef0858d8a53c81ab99fa0467e7e4ec75ed
Chelsi Christmas <c.christmas95@gmail.com>
2020-02-11 10:27:22 -0500

    Set up tab bar controller and 3 tabs, removed instance of main storyboard

---
+++ Unit4Assessment/Controllers/FlashCardsTabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  FlashCardsTabBarController.swift
+//  Unit4Assessment
+//
+//  Created by Chelsi Christmas on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class FlashCardsTabBarController: UITabBarController {
+    
+    private lazy var existingCardsVC: ExistingCardsViewController = {
+     let viewController = ExistingCardsViewController()
+        viewController.tabBarItem = UITabBarItem(title: "Cards", image: UIImage(systemName: "studentdesk"), tag: 0)
+        return viewController
+    }()
+    
+    private lazy var createCardVC: CreateCardViewController = {
+        let viewController = CreateCardViewController()
+        viewController.tabBarItem = UITabBarItem(title: "Create", image: UIImage(systemName: "plus"), tag: 1)
+        return viewController
+    }()
+    
+    private lazy var searchVC: SearchViewController = {
+        let viewController = SearchViewController()
+        viewController.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        return viewController
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         viewControllers = [UINavigationController(rootViewController: existingCardsVC), UINavigationController(rootViewController: createCardVC), UINavigationController(rootViewController: searchVC)]
+
+
+    }
+
+}
